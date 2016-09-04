c2d9c34c4356e933e041ce22d2f6350f406e2e35
Walter Doekes <walter+github@wjd.nu>
2016-09-04 15:33:51 +0200

    Don't crash ongoing calls when receiving SMS.
    According to the bug report by Marcelo Fernandes Vianna:
    > Under some circumstances (if parsing the sms returns -1 in
    > at_response.c), even if sms is disabled in dongle.conf,
    > receiving a sms can still crash an ongoing call.
    
    Thanks for the suggested patch. Refactored and merged.
    Closes #7.

--- at_response.c
+++ at_response.c
@@ -1151,34 +1151,33 @@ static int at_response_cmti (struct pvt* pvt, const char* str)
 // FIXME: check format in PDU mode
 	int index = at_parse_cmti (str);
 
+	if (CONF_SHARED(pvt, disablesms))
+	{
+		ast_log (LOG_WARNING, "[%s] SMS reception has been disabled in the configuration.\n", PVT_ID(pvt));
+		return 0;
+	}
+
 	if (index > -1)
 	{
 		ast_debug (1, "[%s] Incoming SMS message\n", PVT_ID(pvt));
 
-		if (CONF_SHARED(pvt, disablesms))
-		{
-			ast_log (LOG_WARNING, "[%s] SMS reception has been disabled in the configuration.\n", PVT_ID(pvt));
-		}
-		else if(pvt_enabled(pvt))
+		if (pvt_enabled(pvt))
 		{
 			if (at_enque_retrive_sms (&pvt->sys_chan, index, CONF_SHARED(pvt, autodeletesms)))
 			{
 				ast_log (LOG_ERROR, "[%s] Error sending CMGR to retrieve SMS message\n", PVT_ID(pvt));
 				return -1;
 			}
-			else
-			{
-				pvt->incoming_sms = 1;
-			}
+			pvt->incoming_sms = 1;
 		}
-
-		return 0;
 	}
 	else
 	{
 		ast_log (LOG_ERROR, "[%s] Error parsing incoming sms message alert '%s', disconnecting\n", PVT_ID(pvt), str);
 		return -1;
 	}
+
+	return 0;
 }
 
 /*!
