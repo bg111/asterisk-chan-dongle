bg111/asterisk-chan-dongle

commit c24a3150ac61097c6e4b3cca288de66b9d1be2d2

Fix so compilation with different include dirs work.
Now you can do this:

    ./configure --with-astversion=140000 \
        --with-asterisk=$HOME/src/asterisk-14/include
    make clean all

And:

    ./configure --with-astversion=10800 \
        --with-asterisk=$HOME/src/asterisk-1.8/include
    make clean all

Etc..

app.c
@@ -1,7 +1,3 @@
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
-
 #ifdef BUILD_APPLICATIONS
 /*
    Copyright (C) 2009 - 2010
@@ -14,7 +10,8 @@
    bg <bg_one@mail.ru>
 */
 
-#include <asterisk.h>
+#include "ast_config.h"
+
 #include <asterisk/app.h>	/* AST_DECLARE_APP_ARGS() ... */
 #include <asterisk/pbx.h>	/* pbx_builtin_setvar_helper() */
 #include <asterisk/module.h>	/* ast_register_application2() ast_unregister_application() */

manager.c
@@ -1,7 +1,3 @@
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
-
 #ifdef BUILD_MANAGER /* no manager, no copyright */
 /*
    Copyright (C) 2009 - 2010
@@ -13,8 +9,8 @@
 
    bg <bg_one@mail.ru>
 */
+#include "ast_config.h"
 
-#include <asterisk.h>
 #include <asterisk/stringfields.h>		/* AST_DECLARE_STRING_FIELDS for asterisk/manager.h */
 #include <asterisk/manager.h>			/* struct mansession, struct message ... */
 #include <asterisk/strings.h>			/* ast_strlen_zero() */

ast_config.h
@@ -1,0 +1,30 @@
+#ifndef AST_CONFIG_H
+#define AST_CONFIG_H
+
+/* If we want to include config.h, we need to import asterisk.h first
+ * so we can fix the duplicate PACKAGE_* first.
+ * Note that this bug did not show itself until trying to compile with
+ * include files from *outside* /usr/include. Apparently gcc has a
+ * -Wsystem-headers which is *not* enabled by default, causing the
+ * difference. */
+#include <asterisk.h>
+
+#ifdef HAVE_CONFIG_H
+
+/* For some reason, Asterisk exports PACKAGE_* variables which we don't want.
+ * See also: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=733598
+ *
+ * Simply defining ASTERISK_AUTOCONFIG_H to skip importing of the autoconf.h
+ * altogether does not work. It breaks the other Asterisk header files. */
+#undef PACKAGE_BUGREPORT
+#undef PACKAGE_NAME
+#undef PACKAGE_STRING
+#undef PACKAGE_TARNAME
+#undef PACKAGE_URL
+#undef PACKAGE_VERSION
+
+#include "config.h"
+
+#endif /* HAVE_CONFIG_H */
+
+#endif /* AST_CONFIG_H */

at_command.c
@@ -15,11 +15,8 @@
    http://www.makhutov.org
 */
 
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
+#include "ast_config.h"
 
-#include <asterisk.h>
 #include <asterisk/utils.h>
 
 #include "at_command.h"

at_command.h
@@ -4,6 +4,8 @@
 #ifndef CHAN_DONGLE_AT_SEND_H_INCLUDED
 #define CHAN_DONGLE_AT_SEND_H_INCLUDED
 
+#include "ast_config.h"
+
 #include "export.h"		/* EXPORT_DECL EXPORT_DEF */
 #include "dc_config.h"		/* call_waiting_t */
 #include "mutils.h"		/* enum2str_def() ITEMS_OF() */

at_parse.c
@@ -8,9 +8,7 @@
 
    bg <bg_one@mail.ru>
 */
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
+#include "ast_config.h"
 
 #include "memmem.h"
 

at_queue.c
@@ -9,11 +9,8 @@
    Copyright (C) 2010 - 2011
    bg <bg_one@mail.ru>
 */
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
+#include "ast_config.h"
 
-#include <asterisk.h>
 #include <asterisk/utils.h>		/* ast_free() */
 
 #include "at_queue.h"

at_read.c
@@ -9,18 +9,15 @@
    Copyright (C) 2010 - 2011
    bg <bg_one@mail.ru>
 */
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif /* HAVE_CONFIG_H */
-
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE			/* vasprintf() in asterisk/utils.h */
 #endif /* #ifndef _GNU_SOURCE */
 
+#include "ast_config.h"
+
 #include <sys/types.h>
 #include <errno.h>
 
-#include <asterisk.h>
 #include <asterisk/channel.h>		/* ast_waitfor_n_fd() */
 #include <asterisk/logger.h>		/* ast_debug() */
 

commit 428a746e23733c149d49ea3a8857af3fc20cc79f

Unbreak compiled module loading broken in c24a3150.
Fixes #15 but doesn't explain it yet.

app.c
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
 #ifdef BUILD_APPLICATIONS
 /*
    Copyright (C) 2009 - 2010
@@ -10,8 +14,7 @@
    bg <bg_one@mail.ru>
 */
 
-#include "ast_config.h"
-
+#include <asterisk.h>
 #include <asterisk/app.h>	/* AST_DECLARE_APP_ARGS() ... */
 #include <asterisk/pbx.h>	/* pbx_builtin_setvar_helper() */
 #include <asterisk/module.h>	/* ast_register_application2() ast_unregister_application() */

manager.c
@@ -1,3 +1,7 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
 #ifdef BUILD_MANAGER /* no manager, no copyright */
 /*
    Copyright (C) 2009 - 2010
@@ -9,8 +13,8 @@
 
    bg <bg_one@mail.ru>
 */
-#include "ast_config.h"
 
+#include <asterisk.h>
 #include <asterisk/stringfields.h>		/* AST_DECLARE_STRING_FIELDS for asterisk/manager.h */
 #include <asterisk/manager.h>			/* struct mansession, struct message ... */
 #include <asterisk/strings.h>			/* ast_strlen_zero() */