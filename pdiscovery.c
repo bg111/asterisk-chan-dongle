2bd3fc2cc31562826c8341c5584fe26def9ca808
Mostafa Ghadamyari <m.ghadam@gmail.com>
2020-12-23 18:53:44 +0330

    Added E153Du-1 device id ports
    Updated ports for E153Du-1
    IMEI: 868672001033577

--- pdiscovery.c
+++ pdiscovery.c
@@ -65,6 +65,7 @@ static const struct pdiscovery_device device_ids[] = {
 	{ 0x12d1, 0x1001, { 2, 1, /* 0 */ } },		/* E1550 and generic */
 //	{ 0x12d1, 0x1465, { 2, 1, /* 0 */ } },		/* K3520 */
 	{ 0x12d1, 0x140c, { 3, 2, /* 0 */ } },		/* E17xx */
+        { 0x12d1, 0x14ac, { 4, 3, /* 0 */ } },          /* E153Du-1 : thanks mghadam */
 	{ 0x12d1, 0x1436, { 4, 3, /* 0 */ } },		/* E1750 */
 	{ 0x12d1, 0x1506, { 3, 2, /* 0 */ } },		/* E171 firmware 21.x : thanks Sergey Ivanov */
 };
