57e025dda46d2a9e2eba8cb45b75d1b573efc8fb
Walter Doekes <walter+github@wjd.nu>
2022-01-17 09:17:54 +0100

    Fix formatting in error.h

--- error.h
+++ error.h
@@ -4,7 +4,7 @@
 
 #ifndef ERROR_H_INCLUDED
 #define ERROR_H_INCLUDED
-#include "export.h"				/* EXPORT_DECL EXPORT_DEF */
+#include "export.h"  /* EXPORT_DECL EXPORT_DEF */
 #include "mutils.h"
 
 enum error {
@@ -18,7 +18,7 @@ enum error {
 	E_PARSE_UCS2,
 	E_ENCODE_GSM7,
 	E_PACK_GSM7,
-	E_DECODE_GSM7,
+	E_DECODE_GSM7, /* 10 */
 	E_SMSDB,
 	E_QUEUE,
 	E_BUILD_PDU,
@@ -28,7 +28,7 @@ enum error {
 	E_MALFORMED_HEXSTR,
 	E_INVALID_SCA,
 	E_INVALID_TPDU_TYPE,
-	E_PARSE_TPDU,
+	E_PARSE_TPDU, /* 20 */
 	E_INVALID_TIMESTAMP,
 	E_INVALID_CHARSET,
 	E_BUILD_SCA,
@@ -39,12 +39,38 @@ enum error {
 INLINE_DECL const char *error2str(int err)
 {
 	static const char * const errors[] = {
-		"Unknown error", "Device disbaled", "Device not found", "Device disconnected", "Invalid USSD", "Invalid phone number",
-		"Cannot parse UTF-8", "Cannot parse UCS-2", "Cannot encode GSM7", "Cannot pack GSM7", "Cannot decode GSM7", "SMSDB error", "Queue error", "PDU building error",
-		"Can't parse +CMGR response line", "Parsing messages in TEXT mode is not supported anymore; This message should never appear. Nevertheless, if this message appears, please report on GitHub.",
-		"Invalid TPDU length in CMGR PDU status line", "Malformed hex string", "Invalid SCA", "Invalid TPDU type", "Cannot parse TPDU", "Invalid timestamp", "Invalid charset", "Cannot build SCA", "Cannot build phone number", "Input too large"
+		"Unknown error", /* 0 */
+		"Device disabled",
+		"Device not found",
+		"Device disconnected",
+		"Invalid USSD",
+		"Invalid phone number",
+		"Cannot parse UTF-8",
+		"Cannot parse UCS-2",
+		"Cannot encode GSM7",
+		"Cannot pack GSM7",
+		"Cannot decode GSM7", /* 10 */
+		"SMSDB error",
+		"Queue error",
+		"PDU building error",
+		"Can't parse +CMGR response line",
+		("Parsing messages in TEXT mode is not supported anymore; "
+		 "This message should never appear. Nevertheless, if this "
+		 "message appears, please report on GitHub."),
+		"Invalid TPDU length in CMGR PDU status line",
+		"Malformed hex string",
+		"Invalid SCA",
+		"Invalid TPDU type",
+		"Cannot parse TPDU", /* 20 */
+		"Invalid timestamp",
+		"Invalid charset",
+		"Cannot build SCA",
+		"Cannot build phone number",
+		"Input too large"
 	};
-	return enum2str(err, errors, ITEMS_OF(errors));
+	return enum2str(err,
+		 errors,
+		 ITEMS_OF(errors));
 }
 
 EXPORT_DECL __thread int chan_dongle_err;
